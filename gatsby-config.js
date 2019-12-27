23b52fdfc30daad96f3a3e4fee65eff82d7d0062
franck.o.junior <fjunior.aragao@gmail.com>
2019-12-27 17:07:56 -0300

    configurar plugin favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,5 +80,36 @@ module.exports = {
         siteSpeedSampleRate: 10,
       },
     },
+    {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/favicon.png',
+
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'pt-BR',
+        background: 'black',
+        theme_color: 'black',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
   ],
 };
